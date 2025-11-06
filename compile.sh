a8a1a458b2080cb49f04bf86fb5c30616e9fd41b
105071079 <37044030+Jhbai@users.noreply.github.com>
2025-11-07 00:53:42 +0800

    Enhance compile.sh with shared object instructions
    Added comments for shared object compilation instructions.

--- compile.sh
+++ compile.sh
@@ -2,3 +2,8 @@ gcc -c -o theil_sen.o theil_sen.c -mavx # no linking, .o file generate only
 gcc -c -o main.o main.c # no linking, .o file generate only
 gcc -o main main.o theil_sen.o
 ./main
+
+# if want to compile shared object, you can use following commands: (PIC = Position Independent Code)
+# gcc -fPIC -c theil_sen.c -mavx -o theil_sen.o
+# gcc -shared -o libtheil_sen.so theil_sen.o
+# gcc -o main main.c -L. -ltheil_sen (-L. means search from the current folder)
