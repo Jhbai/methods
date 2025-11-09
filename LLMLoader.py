a99ee7af70d05f8f64c400619f283ce42721028e
105071079 <37044030+Jhbai@users.noreply.github.com>
2025-11-10 00:38:18 +0800

    Add LoadModel function to load different models

--- LLMLoader.py
+++ LLMLoader.py
@@ -1,2 +1,9 @@
 from gemma3 import Gemma3Object
 from qwen3 import Qwen3Object
+
+def LoadModel(path):
+    if "gemma3" in path.lower():
+          model = Gemma3Object(path)
+    else:
+        model = Qwen3Object(path)
+    return model
